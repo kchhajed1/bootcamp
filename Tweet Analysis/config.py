86742bff37df63c0006bc07e712f04452ce7d34a
kchhajed1 <kishor.chhajed@gmail.com>
2018-03-28 21:32:24 -0700

    Tweet Analysis

---
+++ Tweet Analysis/config.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+consumer_key = "KQuS0kz6dSOjZHMEzae746Clr"
+consumer_secret = "xcgkRsYykneui9QK3ChuFP3SM326LwcVhY1irVkEx3fyEUnmqi"
+access_token = "43288143-XVdl55MEafyv7Q24DKCqxzQIKsDown7Npc7APFaQE"
+access_token_secret = "mdGSccGFvqL56tyFpoJ7QCKZonWifaX4jtrBWS9HXX10Y"
+
+api_key={"consumer_key":consumer_key,
+         "consumer_secret":consumer_secret,
+         "access_token":access_token,
+         "access_token_secret":access_token_secret
+        }
+
